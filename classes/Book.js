438f6d131187e291fa20656f2ee2b667a4439250
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-13 17:24:45 +0100

    Completed.

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,30 @@
 // import the Media class:
 
+const Media = require("./Media");
+
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static highestRating(bookArr) {
+    let maxRated = null;
+    for (let book of bookArr) {
+      if (!maxRated || maxRated["rating"] < book["rating"]) {
+        maxRated = book;
+      }
+    }
+    return maxRated;
+  }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,29 @@
 // import the Media class:
 
-// create your Movie class:
+const Media = require("./Media");
 
+// create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static longestMovie(movieArr) {
+    let longest = null;
+    for (let movie of movieArr) {
+      if (!longest || longest["duration"] < movie["duration"]) {
+        longest = movie;
+      }
+    }
+    return longest;
+  }
+}
 // don't change below
 module.exports = Movie;
